2fac6c246600c38f579d8fd6283041ed5cf0252c
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-11-13 16:35:48 -0700

    Create EVA1_24_FOR_EACH.java

---
+++ EVA1_24_FOR_EACH/src/eva1_24_for_each/EVA1_24_FOR_EACH.java
@@ -0,0 +1,14 @@
+/*
+Esta practica va a pertenecer a la unidad 2
+ */
+package eva1_24_for_each;
+
+public class EVA1_24_FOR_EACH {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
