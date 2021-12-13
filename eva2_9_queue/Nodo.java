3151c9108a6b990b23ecb5370c1edc5d9ec931e0
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-12-12 19:04:04 -0700

    Create Nodo.java

---
+++ eva2_9_queue/Nodo.java
@@ -0,0 +1,40 @@
+
+package eva2_9_queue;
+
+public class Nodo {
+    private int iDato; // Aqui guardaremos el dato
+    private Nodo siguiente; // Aqui guardaremos los siguientes nodos
+    
+    // Constructores
+    public Nodo(){ // Si no pasamos ningun parametro
+        this.siguiente = null;
+    }
+    public Nodo(int dato){ // si pasamos el dato
+        this.iDato = dato;
+        this.siguiente = null;
+    }
+
+    //  SETTERS Y GETTERS
+    
+    // getter de dato
+    public int getiDato() {
+        return iDato;
+    }
+
+    // Setter de dato
+    public void setiDato(int iDato) {
+        this.iDato = iDato;
+    }
+
+    // Getter del nodo
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    // Setter del nodo
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    
+}
