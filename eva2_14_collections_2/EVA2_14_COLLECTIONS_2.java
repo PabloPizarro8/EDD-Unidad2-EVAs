6950729bf26bc0b3de9c46aee75ea7ffb5b467c9
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-12-12 19:12:39 -0700

    Create EVA2_14_COLLECTIONS_2.java

---
+++ eva2_14_collections_2/EVA2_14_COLLECTIONS_2.java
@@ -0,0 +1,14 @@
+
+package eva2_14_collections_2;
+
+import java.util.Queue;
+
+public class EVA2_14_COLLECTIONS_2 {
+    
+    public static void main(String[] args) {
+        Queue cola = new Queue();
+        
+        
+    }
+    
+}
