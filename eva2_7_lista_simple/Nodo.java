e082c4b714493d88daf220f5c51eb06e21bff0bf
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-12-12 19:07:12 -0700

    Update Nodo.java

--- eva2_7_lista_simple/Nodo.java
+++ eva2_7_lista_simple/Nodo.java
@@ -2,29 +2,36 @@
 package eva2_7_lista_simple;
 
 public class Nodo {
-    private int iDato;
-    private Nodo siguiente;
+    private int iDato; // Aqui guardaremos el dato
+    private Nodo siguiente; // Aqui guardaremos los siguientes nodos
     
-    public Nodo(){
+    // Constructores
+    public Nodo(){ // Si no pasamos ningun parametro
         this.siguiente = null;
     }
-    public Nodo(int dato){
+    public Nodo(int dato){ // si pasamos el dato
         this.iDato = dato;
         this.siguiente = null;
     }
 
+    //  SETTERS Y GETTERS
+    
+    // getter de dato
     public int getiDato() {
         return iDato;
     }
 
+    // Setter de dato
     public void setiDato(int iDato) {
         this.iDato = iDato;
     }
 
+    // Getter del nodo
     public Nodo getSiguiente() {
         return siguiente;
     }
 
+    // Setter del nodo
     public void setSiguiente(Nodo siguiente) {
         this.siguiente = siguiente;
     }
