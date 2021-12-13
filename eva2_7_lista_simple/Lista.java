d9d9853261754a54f58ae0bd5b89a26d7e656dd4
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-12-12 19:07:16 -0700

    Update Lista.java

--- eva2_7_lista_simple/Lista.java
+++ eva2_7_lista_simple/Lista.java
@@ -3,65 +3,170 @@ package eva2_7_lista_simple;
 
 public class Lista {
     private Nodo inicio, finnal;
+    private int tamano; // Contador de nodos
 
     public Lista() {
         inicio = null; // LISTA VACIA
         finnal = null; // Posicion final de la lista
+        tamano = 0;
     }
+    
     // AGREGAR NODOS
-    public void add(Nodo nuevoNodo){
-        // HAY NODOS EN LA LISTA?
-        if (inicio == null) { // LISTA VACIA
-            inicio = nuevoNodo; // conectamos el primer nodo a la lista
-            finnal = nuevoNodo;
-        }else{// Lista con nodos
+    public void add(int valor){
+        Nodo nuevoNodo = new Nodo(valor);
+        // ¿HAY NODOS EN LA LISTA?
+        if (inicio == null) {  // LISTA VACIA
+            inicio = nuevoNodo;  // conectamos el primer nodo a la lista
+            finnal = nuevoNodo;  // Conectamos el nuevo nodo al finale de la lista
+        }else{  // Lista con nodos
             /*
             // Hay que movernos al final de la lista 
             // Usando temp, Nodo por nodo hasta llegar al final
-            Nodo temp = inicio; // Aqui empiezo
-            // While
-            while(temp.getSiguiente() != null){ // Mientras temp sea diferente a null, nos vamos a mover
-                temp = temp.getSiguiente(); // Mover a temp al siguiente nodo
+            Nodo temp = inicio;  // Aqui empiezo
+            
+            // While|| Consume demasiado, no es eficiente
+            while(temp.getSiguiente() != null){  // Mientras temp sea diferente a null, nos vamos a mover
+                temp = temp.getSiguiente();  // Mover a temp al siguiente nodo
             }
-            temp.setSiguiente(nuevoNodo); // Conectamos al final de la lista al nuevo nodo
+            
+            temp.setSiguiente(nuevoNodo);  // Conectamos al final de la lista al nuevo nodo
 */
             // Agregar el nodo al final de la lista
             finnal.setSiguiente(nuevoNodo);
             finnal = nuevoNodo;
             
         }
+        tamano++;
     }
+    
     // IMPRIMIR LA LISTA
     public void printList(){
-        Nodo temp = inicio; // Aqui empezamos
+        Nodo temp = inicio; // Empezamos desde el nodo inicial
         
+        // Pasaremos por cada elemento de la lista hasta que encontremos un null 
         while(temp != null){
             System.out.print(temp.getiDato() + " - ");
             temp = temp.getSiguiente(); // Mover a temp al siguiente nodo
         }
+        System.out.println("");
     }
+    
     // LIMPIAR LA LISTA
-    public void clearList(){
-        inicio = null;
+    public void limpiar(){
+        inicio = null; // Mandamos inicio a valor null, ocasionando que se borren todos 
+            // los demas nodos
+        finnal = null; // Por si
+        tamano = 0;
     }
+    
     // CONTAR LOS NODOS
-    public int lenght(){
-        int iCont = 0;
-        Nodo temp = inicio; // Aqui empezamos
+    // Es ineficiente, cambiara en el futuro
+    // Ya cambio, ahora parece mas un Getter XD
+    public int tamanoLista(){/*
+        int iCont = 0;  // Variable para contar, iniciada en 0
+        Nodo temp = inicio;  // Aqui empezamos
         
+        // Mientras no este "temp" en null, nos quedamos en el ciclo:
         while(temp != null){
-            iCont++;
-            temp = temp.getSiguiente(); // Mover a temp al siguiente nodo
+            iCont++;  // incrementamos el contador
+            temp = temp.getSiguiente();  // Mover a temp al siguiente nodo
         }
-        return iCont;
+        return iCont; // Imrpimimos el contador, que sera el tamaño del litsa*/
+        return tamano;
+    }
+    
+    // La lista esta vacia: true, con nodos: false
+    public boolean isEmpty(){
+        if(inicio == null) 
+            return true;
+        else
+            return false;
     }
     
     // RECUPERAR UN VALOR DE LA LISTA:
-    public int getPosicion(int iPos){
-        Nodo temp = inicio;
+    public int getPosicion(int iPos) throws Exception{
+        // Verificacion 
+        // Lista este vacia
+        if(isEmpty()){
+            throw new Exception("No hay valores almacenados en la lista!!");
+        }
+        else{// Posicion que no exista: negativa o excesiva
+            // Pos tiene que estar entre cero y n-1
+            // Si iPos es menor a cero, o mayor igual a N, entonces: 
+            if( (iPos < 0) || (iPos >= tamanoLista() ) ){
+                throw new Exception("Posicion fuera de rango");
+            }
+        }
+        Nodo temp = inicio; // Empezamos en el nodo inicial
+        // El ciclo for es para pasar por cada nodo hasta la posicion indicada
         for (int i = 0; i < (iPos); i++) {
-            temp = temp.getSiguiente();
+            temp = temp.getSiguiente();  // Guardaremos cada nodo en temp, hasta llegar al deseado
         }
         return temp.getiDato();
+    }
+    
+    // Agregar un nodo en x posicion de la lista, y que esta se extienda
+    public void insertarEn(int iPos, int valor){
+        Nodo nuevo = new Nodo(valor);
+        // En caso de querer insertarlo al inicio
+        if(iPos == 0){
+            nuevo.setSiguiente(inicio);
+            inicio = nuevo;
+        }
+        // En caso de que sea en cualquier otra posicion
+        else{
+            Nodo temp = inicio;
+            // Movemos temp a una posicion anterior a la deseada
+            for (int i = 0; i < (iPos - 1); i++) {
+                temp = temp.getSiguiente();  // Guardaremos cada nodo en temp, hasta 
+                                            // llegar al nodo previo al deseado
+            }
+            // Colocamos los nodos siguientes a la posicion en nuevo.setSiguiente()
+            nuevo.setSiguiente(temp.getSiguiente());
+            // y ponemos nuevo en el siguiente de temp
+            temp.setSiguiente(nuevo);
+        }
+        tamano++;
+    }
+    
+    // Borrar en cualquier posicion
+    public void borrarEn(int iPos){
+        if(tamanoLista() == 1){
+            limpiar();
+        }
+        else{
+            // en caso de que sea la posicion 0
+            if(iPos == 0){
+                inicio = inicio.getSiguiente();
+            }
+            // en caso de que sea cualquier otra posicion diferente de 0
+            else{
+                Nodo temp = inicio;
+                for (int i = 0; i < (iPos - 1); i++) {
+                    temp = temp.getSiguiente();  // Guardaremos cada nodo en temp, hasta llegar al deseado
+                }
+                // Algo confuso aqui, Pablo del futuro
+                /*
+                Pasaremos de temp el siguiente del siguiente a setSiguiente de temp
+                para que el primer getSiquiente se borre
+                este siguiente es la posicion que queremos borrar
+                
+                ejemplo, si queremos eliminar el 2 (para la lista es el 3) 
+                entonces temp se quedara en 1, buscaremos el siguiente del siguiente de este
+                que seria el siguiente de 2, o sea el 3, y este lo mandaremos al siguiente de
+                1, que es 2, eliminando este ultimo
+                
+                quedandonos solo los puestos 0 1 3 4
+                
+                atte Pablo del pasado
+                */
+                temp.setSiguiente(temp.getSiguiente().getSiguiente());
+                
+                if(iPos == tamanoLista() - 1){ // Verificar si es el ultimo nodo de la lista
+                    finnal = temp;
+                }
+            }
+        }
+        tamano--;
     }
 }
